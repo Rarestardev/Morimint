797466416d10c35bf3cec7aab811249d4739ceea
SocRPioN <Rarestar.dev@gmail.com>
2024-07-26 19:17:44 +0330

    levelUserComplete

--- app/src/main/java/com/rarestardev/morimint/ApplicationSetup/ApplicationManager.java
+++ app/src/main/java/com/rarestardev/morimint/ApplicationSetup/ApplicationManager.java
@@ -1,103 +1,122 @@
 package com.rarestardev.morimint.ApplicationSetup;
 
-
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.PorterDuff;
-import android.os.Handler;
-import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.AppCompatImageView;
-import androidx.core.content.ContextCompat;
 
 import com.rarestardev.morimint.R;
 
-public class ProgramManager {
-
-    ProgressBar progressBar;
-    Context context;
-    TextView tvLevel;
-    AppCompatImageView Coin,Turbo;
-
-
-    int energy;
-    int mint = 1;
-    int minEnergy;
-    int progressStatus;
+public class ChargeEnergyCounter {
+    int value;
+    int maxValue;
+    int minValue;
+    int step;
+    int minter;
     long totalBalance;
-    private static final String LOG_TAG = "ProgressManager";
-
-    /* private static final long[] LEVEL_COIN = {200000,700000,1500000,5000000,8000000,15000000,25000000,35000000,50000000,70000000,90000000,
-    120000000,150000000,200000000,300000000};*/
+    int progressStatus;
 
     private static final long[] LEVEL_COIN = {100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500};
 
-    public ProgramManager(ProgressBar progressBar, Context context, TextView tvLevel, AppCompatImageView Coin, AppCompatImageView Turbo) {
-        this.progressBar = progressBar;
-        this.context = context;
-        this.tvLevel = tvLevel;
-        this.Coin = Coin;
-        this.Turbo = Turbo;
+    public ChargeEnergyCounter() {
+        this.value = 1000;
+        this.maxValue = 1000;
+        this.minValue = 0;
+        this.step = 3;
+        this.minter = 1;
     }
 
-    public void initTotalCoin(long coin){
-        totalBalance = coin;
-        Log.i(LOG_TAG,"getCoin :" + totalBalance);
-        initLevelValues();
+    public void increment() {
+        if (value < maxValue) {
+            value += step;
+        }
+        if (value > maxValue) {
+            value = maxValue;
+        }
+    }
 
-       // changeProgressBarColor();
+    public void decrement(int mint) {
+        if (value >= 1) {
+            value -= mint;
+            if (value < minValue) {
+                value = minValue;
+            }
+        }
+    }
+
+    public boolean mintStop(int mint) {
+        return value < mint;
+    }
+
+    public void increase(int currentTime, int last_energy) {
+        int added_energy = currentTime * step;
+
+        value = Math.min(last_energy + added_energy, maxValue);
+        if (value > maxValue){
+            value = maxValue;
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getMaxValue() {
+        return maxValue;
+    }
+
+    public int getMinter() {
+        return minter;
     }
 
     @SuppressLint("UseCompatLoadingForDrawables")
-    public void initLevelValues(){
+    public void initLevelValues(Context context,long coin , TextView tvLevel, ProgressBar progressBar, AppCompatImageView Coin, AppCompatImageView Turbo){
+        totalBalance = coin;
         if (totalBalance < LEVEL_COIN[0]){
             tvLevel.setText(String.valueOf(1));
             progressBar.setMax((int) LEVEL_COIN[0]);
-            initProgressBarValues(LEVEL_COIN[0]);
+            initProgressBarValues(LEVEL_COIN[0],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_one));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_one));
 
         }else if (totalBalance > LEVEL_COIN[0] && totalBalance <= LEVEL_COIN[1]){
             tvLevel.setText(String.valueOf(2));
-            energy = 2000;
             progressBar.setMax((int) LEVEL_COIN[1]);
-            initProgressBarValues(LEVEL_COIN[1]);
+            initProgressBarValues(LEVEL_COIN[1],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_two));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_two));
+            maxValue = 2000;
 
         }else if (totalBalance > LEVEL_COIN[1] && totalBalance <= LEVEL_COIN[2]){
             tvLevel.setText(String.valueOf(3));
-            energy = 3000;
-            mint = 3;
             progressBar.setMax((int) LEVEL_COIN[2]);
-            initProgressBarValues(LEVEL_COIN[2]);
+            initProgressBarValues(LEVEL_COIN[2],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_three));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_three));
 
+            maxValue = 3000;
+
         }else if (totalBalance > LEVEL_COIN[2] && totalBalance <= LEVEL_COIN[3]){
             tvLevel.setText(String.valueOf(4));
-            energy = 4000;
-            mint = 4;
+
             progressBar.setMax((int) LEVEL_COIN[3]);
-            initProgressBarValues(LEVEL_COIN[3]);
+            initProgressBarValues(LEVEL_COIN[3],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_four));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_four));
 
         }else if (totalBalance > LEVEL_COIN[3] && totalBalance <= LEVEL_COIN[4]){
             tvLevel.setText(String.valueOf(5));
-            energy = 5000;
-            mint = 5;
             progressBar.setMax((int) LEVEL_COIN[4]);
-            initProgressBarValues(LEVEL_COIN[4]);
+            initProgressBarValues(LEVEL_COIN[4],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_five));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_five));
 
         }else if (totalBalance > LEVEL_COIN[4] && totalBalance <= LEVEL_COIN[5]){
             tvLevel.setText(String.valueOf(6));
             progressBar.setMax((int) LEVEL_COIN[5]);
-            initProgressBarValues(LEVEL_COIN[5]);
+            initProgressBarValues(LEVEL_COIN[5],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_six));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_six));
 
@@ -105,7 +124,7 @@ public class ProgramManager {
             tvLevel.setText(String.valueOf(7));
             progressStatus = 0;
             progressBar.setMax((int) LEVEL_COIN[6]);
-            initProgressBarValues(LEVEL_COIN[6]);
+            initProgressBarValues(LEVEL_COIN[6],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_seven));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_seven));
 
@@ -113,7 +132,7 @@ public class ProgramManager {
             tvLevel.setText(String.valueOf(8));
             progressStatus = 0;
             progressBar.setMax((int) LEVEL_COIN[7]);
-            initProgressBarValues(LEVEL_COIN[7]);
+            initProgressBarValues(LEVEL_COIN[7],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_eight));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_eight));
 
@@ -121,7 +140,7 @@ public class ProgramManager {
             tvLevel.setText(String.valueOf(9));
             progressStatus = 0;
             progressBar.setMax((int) LEVEL_COIN[8]);
-            initProgressBarValues(LEVEL_COIN[8]);
+            initProgressBarValues(LEVEL_COIN[8],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_nine));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_nine));
 
@@ -129,7 +148,7 @@ public class ProgramManager {
             tvLevel.setText(String.valueOf(10));
             progressStatus = 0;
             progressBar.setMax((int) LEVEL_COIN[9]);
-            initProgressBarValues(LEVEL_COIN[9]);
+            initProgressBarValues(LEVEL_COIN[9],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_ten));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_ten));
 
@@ -137,7 +156,7 @@ public class ProgramManager {
             tvLevel.setText(String.valueOf(11));
             progressStatus = 0;
             progressBar.setMax((int) LEVEL_COIN[10]);
-            initProgressBarValues(LEVEL_COIN[10]);
+            initProgressBarValues(LEVEL_COIN[10],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_eleven));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_eleven));
 
@@ -145,7 +164,7 @@ public class ProgramManager {
             tvLevel.setText(String.valueOf(12));
             progressStatus = 0;
             progressBar.setMax((int) LEVEL_COIN[11]);
-            initProgressBarValues(LEVEL_COIN[11]);
+            initProgressBarValues(LEVEL_COIN[11],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_twelve));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_twelve));
 
@@ -153,7 +172,7 @@ public class ProgramManager {
             tvLevel.setText(String.valueOf(13));
             progressStatus = 0;
             progressBar.setMax((int) LEVEL_COIN[12]);
-            initProgressBarValues(LEVEL_COIN[12]);
+            initProgressBarValues(LEVEL_COIN[12],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_thirteen));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_thirteen));
 
@@ -161,7 +180,7 @@ public class ProgramManager {
             tvLevel.setText(String.valueOf(14));
             progressStatus = 0;
             progressBar.setMax((int) LEVEL_COIN[13]);
-            initProgressBarValues(LEVEL_COIN[13]);
+            initProgressBarValues(LEVEL_COIN[13],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_fourteen));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_fourteen));
 
@@ -169,7 +188,7 @@ public class ProgramManager {
             tvLevel.setText(String.valueOf(15));
             progressStatus = 0;
             progressBar.setMax((int) LEVEL_COIN[14]);
-            initProgressBarValues(LEVEL_COIN[14]);
+            initProgressBarValues(LEVEL_COIN[14],progressBar);
             Coin.setImageDrawable(context.getDrawable(R.drawable.level_fifteen));
             Turbo.setImageDrawable(context.getDrawable(R.drawable.level_fifteen));
 
@@ -182,12 +201,7 @@ public class ProgramManager {
         }
     }
 
-
-
-
-
-
-    public void initProgressBarValues(long currentCoin){
+    private void initProgressBarValues(long currentCoin,ProgressBar progressBar){
         if (totalBalance < currentCoin){
             progressStatus = (int) totalBalance;
         }
@@ -205,63 +219,5 @@ public class ProgramManager {
                 e.printStackTrace();
             }
         }).start();
-    }
-
-
-
-    public int getEnergy() {
-        return energy;
-    }
-
-    public int getMint() {
-        return mint;
-    }
-
-    public void setEnergy(int energy) {
-        this.energy = energy;
-    }
-
-    public void setMint(int mint) {
-        this.mint = mint;
-    }
-
-    public int getMinEnergy() {
-        return minEnergy;
-    }
-
-    public void setMinEnergy(int minEnergy) {
-        this.minEnergy = minEnergy;
-    }
-
-
-
-
-
-
-    private void changeProgressBarColor() {
-        int progress = progressBar.getProgress();
-        int colorId;
-        if (progress < 10) {
-            colorId = R.color.white;
-        } else if (progress < 20) {
-            colorId = R.color.black;
-        } else if (progress < 30) {
-            colorId = R.color.JackpotBonusAnim1;
-        } else if (progress < 40) {
-            colorId = R.color.BaseColor;
-        } else if (progress < 50) {
-            colorId = R.color.Gray;
-        } else if (progress < 60) {
-            colorId = R.color.Fields;
-        } else if (progress < 70) {
-            colorId = R.color.JackpotBonusAnim2;
-        } else if (progress < 80) {
-            colorId = R.color.JackpotBonusAnim1;
-        } else if (progress < 90) {
-            colorId = R.color.Views;
-        } else {
-            colorId = R.color.JackpotBonusAnim3;
-        }
-        progressBar.getProgressDrawable().setColorFilter(ContextCompat.getColor(context, colorId), PorterDuff.Mode.SRC_ATOP);
     }
 }
--- app/src/main/java/com/rarestardev/morimint/Constants/EnergyValue.java
+++
@@ -1,8 +0,0 @@
-package com.rarestardev.morimint.Constants;
-
-public class EnergyValue {
-
-    public static int Energy = 1000;
-
-    public static int Mint = 1;
-}
